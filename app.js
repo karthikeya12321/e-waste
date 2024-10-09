e9c5f017e89b1b79f1b26da201541a12e318ab30
karthik <karthikeyaputta9@gmail.com>
2024-10-09 14:10:53 +0530

    adding intital files

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "public/views"))
+app.use(express.static(path.join(__dirname, "public/css")));
 app.use(express.static(path.join(__dirname, "public/")));
 app.use(cookieParser());
 
